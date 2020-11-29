07b5ac2aa5b4138ce063cbbf5626f7878737dd5b
Crowdbotics <team@crowdbotics.com>
2020-11-29 22:16:24 +0000

    Update models

--- backend/incredix_23061/wsgi.py
+++ backend/incredix_23061/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'incredix_23061.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "incredix_23061.settings")
 
 application = get_wsgi_application()
